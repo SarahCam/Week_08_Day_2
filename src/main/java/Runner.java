1f3787c64f0a45f509598c77c5542bb94ccbf2b2
Sarah Campbell <scampbell.home@btinternet.com>
2018-03-13 18:41:03 +0000

    Added delete() method to DBHelper class.

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,6 +2,8 @@ import db.DBHelper;
 import models.Folder;
 import models.File;
 
+import java.util.List;
+
 public class Runner {
 
     public static void main(String[] args) {
@@ -13,6 +15,13 @@ public class Runner {
         DBHelper.save(file1);
         File file2 = new File("Game", "java", 1024, folder1);
         DBHelper.save(file2);
+
+        file2.setExtension("rb");
+        DBHelper.update(file2);
+        List<File> files1 = DBHelper.getAll(File.class);
+
+        DBHelper.delete(file1);
+        List<File> files2 = DBHelper.getAll(File.class);
 
     }
 }
